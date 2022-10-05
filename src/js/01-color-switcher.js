fe5c5619c9ec3f42c3e8638fddea44e720d362ff
SofiiaKorost <korostenska@icloud.com>
2022-10-05 23:32:28 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+let timerI = null;
+
+startButton.addEventListener('click', onButtonStart);
+stopButton.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+    timerI = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        startButton.setAttribute('disabled', 'disabled');
+    }, 1000);
+}
+
+function onButtonStop() {
+    clearInterval(timerI);
+    startButton.removeAttribute('disabled', 'disabled')
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+} x
 
